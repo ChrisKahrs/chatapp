823bacc3f3c2cb53d2de4de822b237529080f52e
Chris Kahrs <chriskahrs@msn.com>
2020-05-27 15:31:47 -0400

    chatapp 7

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from models import *
 
 from wtforms_fields import *
@@ -18,13 +18,26 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
-        # add user
+        # add user to db
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        return "Inserted into DB!"
+        return redirect(url_for('login'))
 
     return render_template('index.html', form=reg_form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+
+    login_form = LoginForm()
+
+    # Allow Login if validataion success
+    if login_form.validate_on_submit():
+        return "Logged in, finally!"
+    
+    return render_template("login.html", form=login_form)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- wtforms_fields.py
+++ wtforms_fields.py
@@ -3,6 +3,19 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired,  Length, EqualTo, ValidationError
 from models import User
 
+def  invalid_credentials(form, field):
+    """ Username and password checker """
+
+    username_entered = form.username.data
+    password_entered = field.data
+
+    # check credentials are valid
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError("Username or password incorrect!")
+    elif password_entered != user_object.password:
+        raise ValidationError("Username or password incorrect!")
+
 class RegistrationForm(FlaskForm):
     """ Registration form """
 
@@ -24,3 +37,12 @@ class RegistrationForm(FlaskForm):
         user_object = User.query.filter_by(username=username.data).first()
         if user_object:
             raise ValidationError("Username already exists in db!")
+
+class LoginForm(FlaskForm):
+    """ Login Form """
+
+    username = StringField('username_label', validators=[InputRequired(message="Username Required")])
+
+    password = PasswordField('password_label', validators=[InputRequired(message="Password Required"), invalid_credentials])
+
+    submit_button = SubmitField('Login')
